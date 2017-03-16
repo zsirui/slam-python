6e5815c879801d1f0b33df31ba2f34dfccbcaeb9
Ray <zsirui@126.com>
2017-03-16 10:03:38 +0800

    Update

--- 3/joinPointCloud.py
+++ 3/joinPointCloud.py
@@ -10,16 +10,20 @@ DepthFileNameList = ['./data/depth1.png', './data/depth2.png']
 CalibrationDataFile = './calibration/asus/camera.yml'
 CloudFilename = './data/cloud.pcd'
 
-CameraIntrinsicData, DistortionCoefficients = readyaml.parseYamlFile(CalibrationDataFile)
-camera = slamBase.CameraIntrinsicParameters(CameraIntrinsicData[0][2], CameraIntrinsicData[1][2], CameraIntrinsicData[0][0], CameraIntrinsicData[1][1], 1000.0)
-frame1 = slamBase.Frame(RGBFileNameList[0], DepthFileNameList[0])
-frame2 = slamBase.Frame(RGBFileNameList[1], DepthFileNameList[1])
-pnp = slamBase.SolvePnP(DistortionCoefficients, CameraIntrinsicData, camera, frame1, frame2)
+def main():
+	CameraIntrinsicData, DistortionCoefficients = readyaml.parseYamlFile(CalibrationDataFile)
+	camera = slamBase.CameraIntrinsicParameters(CameraIntrinsicData[0][2], CameraIntrinsicData[1][2], CameraIntrinsicData[0][0], CameraIntrinsicData[1][1], 1000.0)
+	frame1 = slamBase.Frame(RGBFileNameList[0], DepthFileNameList[0])
+	frame2 = slamBase.Frame(RGBFileNameList[1], DepthFileNameList[1])
+	pnp = slamBase.SolvePnP(DistortionCoefficients, CameraIntrinsicData, camera, frame1, frame2)
 
-p0, c0 = slamBase.imageToPointCloud(RGBFileNameList[0], DepthFileNameList[0], camera)
-p1, c1 = slamBase.imageToPointCloud(RGBFileNameList[1], DepthFileNameList[1], camera)
-colors = c0 + c1
-p = slamBase.transformPointCloud(p0, pnp.T)
-pointcloud = slamBase.addPointCloud(p, p1)
-save(pointcloud, CloudFilename, format = 'pcd')
-slamBase.addColorToPCDFile(CloudFilename, colors)
+	p0, c0 = slamBase.imageToPointCloud(RGBFileNameList[0], DepthFileNameList[0], camera)
+	p1, c1 = slamBase.imageToPointCloud(RGBFileNameList[1], DepthFileNameList[1], camera)
+	colors = c0 + c1
+	p = slamBase.transformPointCloud(p0, pnp.T)
+	pointcloud = slamBase.addPointCloud(p, p1)
+	save(pointcloud, CloudFilename, format = 'pcd')
+	slamBase.addColorToPCDFile(CloudFilename, colors)
+
+if __name__ == '__main__':
+	main()
