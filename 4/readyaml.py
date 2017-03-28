c6b5c79b2cc887bdf1b64d20a4cb4025363cb3df
Ray <zsirui@126.com>
2017-03-28 09:26:08 +0800

    Create readyaml.py

---
+++ 4/readyaml.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import yaml, sys
+import numpy as np
+
+def fixYamlFile(filename):
+	with open(filename, 'rb') as f:
+		lines = f.readlines()
+	if lines[0] != '%YAML 1.0\n':
+		lines[0] = '%YAML 1.0\n'
+	for line in lines:
+		if ' !!opencv-matrix' in line:
+			lines[lines.index(line)] = line.split(' !!opencv-matrix')[0] + '\n'
+	with open(filename, 'wb') as fw:
+		fw.writelines(lines)
+
+def parseYamlFile(filename):
+	fixYamlFile(filename)
+	f = open(filename)
+	x = yaml.load(f)
+	f.close()
+	CameraIntrinsicData = np.array(x['camera_matrix']['data'], dtype = np.float32)
+	DistortionCoefficients = np.array(x['distortion_coefficients']['data'], dtype = np.float32)
+	return (CameraIntrinsicData.reshape(3, 3), DistortionCoefficients)
