2e6eea53e52119d6069cbeadbf514068c3bcbe4d
Ray <zsirui@126.com>
2017-04-07 18:25:51 +0800

    Update test.cpp

--- 4/test.cpp
+++ 4/test.cpp
@@ -1,41 +1,59 @@
-
-// Eigen
-// #include <Eigen/Core>
-// #include <Eigen/Geometry>
-#include <fstream>
-#include <vector>
-#include <map>
-
-using namespace std;
+#include <iostream>
+#include <iomanip>
 
 //PCL
+#include <pcl/io/io.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/common/transforms.h>
 #include <pcl/visualization/cloud_viewer.h>
-#include "boost/python.hpp"
+
+//boost
+#include <boost/python.hpp>
+#include <boost/python/list.hpp>
+
+using namespace std;
 using namespace boost::python;
 
 // 类型定义
 typedef pcl::PointXYZRGBA PointT;
 typedef pcl::PointCloud<PointT> PointCloud;
-PointCloud::Ptr ToPCL(double x, double y, double z, int r, int g, int b);
 
-PointCloud::Ptr ToPCL(double x, double y, double z, int r, int g, int b)
+PointT toPointXYZRGBA(long double x, long double y, long double z, uint8_t r, uint8_t g, uint8_t b);
+void showList(const boost::python::list& pyList);
+void showCloud(const boost::python::object& cloud);
+
+PointT toPointXYZRGBA(long double x, long double y, long double z, uint8_t r, uint8_t g, uint8_t b)
 {
 	PointT p;
-	PointCloud::Ptr cloud ( new PointCloud );
 	p.x = x;
 	p.y = y;
 	p.z = z;
 	p.r = r;
 	p.g = g;
 	p.b = b;
-	cloud->points.push_back( p );
-	return cloud;
+	return p;
 }
 
-BOOST_PYTHON_MODULE(Test)
+void showList(const boost::python::list& pyList)
 {
-	def("ToPCL", ToPCL);
+	PointT p;
+	p = toPointXYZRGBA(boost::python::extract<long double>(pyList[0]), boost::python::extract<long double>(pyList[1]), boost::python::extract<long double>(pyList[2]), boost::python::extract<uint8_t>(pyList[3]), boost::python::extract<uint8_t>(pyList[4]), boost::python::extract<uint8_t>(pyList[5]));
+	cout<<"p.x "<<p.x<<endl;
+	cout<<"p.y "<<p.y<<endl;
+	cout<<"p.z "<<p.z<<endl;
+	cout<<"p.rgba "<<p.rgba<<endl;
+}
+
+void showCloud(PointCloud::Ptr cloud)
+{
+	pcl::visualization::CloudViewer viewer("viewer");
+	viewer.showCloud( cloud );
+}
+
+BOOST_PYTHON_MODULE(lib_pcl)
+{
+	def("showList", showList);
+	// def("showCloud", showCloud);
+	// class_("PointCloud",init())
 }
