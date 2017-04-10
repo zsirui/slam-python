52b7ed510960e110959d8f908c2f4bc19daed1cd
Ray <zsirui@126.com>
2017-04-10 18:16:43 +0800

    Create lib_pcl.cpp

---
+++ 4/lib_pcl.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <iomanip>
+
+//PCL
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/common/transforms.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+//boost
+#include <boost/python.hpp>
+#include <boost/python/list.hpp>
+
+using namespace std;
+using namespace boost::python;
+
+// 类型定义
+typedef pcl::PointXYZRGBA PointT;
+typedef pcl::PointCloud<PointT> PointCloud;
+
+PointT toPointXYZRGBA(long double x, long double y, long double z, uint8_t r, uint8_t g, uint8_t b);
+void showList(const boost::python::list& pyList);
+// void showCloud(PointCloud::Ptr cloud);
+
+PointT toPointXYZRGBA(long double x, long double y, long double z, uint8_t r, uint8_t g, uint8_t b)
+{
+	PointT p;
+	p.x = x;
+	p.y = y;
+	p.z = z;
+	p.r = r;
+	p.g = g;
+	p.b = b;
+	return p;
+}
+
+void showList(const boost::python::list& pyList)
+{
+	PointT p;
+	p = toPointXYZRGBA(boost::python::extract<long double>(pyList[0]), boost::python::extract<long double>(pyList[1]), boost::python::extract<long double>(pyList[2]), boost::python::extract<uint8_t>(pyList[3]), boost::python::extract<uint8_t>(pyList[4]), boost::python::extract<uint8_t>(pyList[5]));
+	cout<<"p.x "<<p.x<<endl;
+	cout<<"p.y "<<p.y<<endl;
+	cout<<"p.z "<<p.z<<endl;
+	cout<<"p.rgba "<<p.rgba<<endl;
+}
+
+// void showCloud(PointCloud::Ptr cloud)
+// {
+// 	pcl::visualization::CloudViewer viewer("viewer");
+// 	viewer.showCloud( cloud );
+// }
+
+typedef boost::shared_ptr < pcl::PointXYZRGBA > PointXYZRGBA_ptr;
+typedef boost::shared_ptr < PointCloud::Ptr > PointCloud_ptr;
+// BOOST_PYTHON_FUNCTION_OVERLOADS(showCloud_overloads,pcl::visualization::CloudViewer::showCloud, 1,2);
+BOOST_PYTHON_MODULE(lib_pcl)
+{
+	def("showList", showList);
+	// def("toPointXYZRGBA", toPointXYZRGBA, return_value_policy<reference_existing_object>());
+	class_<pcl::PointXYZRGBA>("PointXYZRGBA")
+		.def_readwrite("x", &pcl::PointXYZRGBA::x)
+		.def_readwrite("y", &pcl::PointXYZRGBA::y)
+		.def_readwrite("z", &pcl::PointXYZRGBA::z)
+		.def_readwrite("rgba", &pcl::PointXYZRGBA::rgba);
+	// class_<pcl::visualization::CloudViewer, boost::noncopyable>("CloudViewer", init<std::string>())
+	// 	.def("showCloud", (void (*)(pcl::visualization::CloudViewer::ColorCloud::ConstPtr, std::string&))&pcl::visualization::CloudViewer::showCloud, showCloud_overloads());
+	register_ptr_to_python <PointXYZRGBA_ptr>();
+	register_ptr_to_python <PointCloud_ptr>();
+}
